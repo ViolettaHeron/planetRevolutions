7da0f29a3a30dfea1ef8692b36f487784339ab39
Violette <v.heron@qwant.com>
2016-06-30 16:45:41 +0200

    stars generator

--- fonctions.js
+++ fonctions.js
@@ -1,1 +1,29 @@
+var nbStars = 1000;
 
+function init(){
+	var can = document.getElementById('sky')
+	drawStars(can);
+}
+
+function drawStars(can){
+	var ctx = can.getContext("2d");
+	
+
+	var maxHeight = can.height;
+	var maxWidth = can.width;
+
+	for(var i = 0;i<nbStars;i++){
+		var hue=Math.floor(Math.random()*45+173), sat=Math.floor(Math.random()*10+56),lum=Math.floor(Math.random()*20+80)
+		starsColor = "hsl("+hue+", "+sat+"%, "+lum+"%)"
+
+		ctx.fillStyle = starsColor;
+		var x=Math.random()*maxWidth, y=Math.random()*maxHeight;
+		var l=Math.random()<0.99?Math.random()*2:Math.random()*4;
+
+		ctx.fillRect(x,y,l,l);
+	}
+
+
+	console.log(starsColor);
+
+}
