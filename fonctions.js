4dd11db76b518190056486517615f4b2fd5adc7b
unknown <heron.violette@gmail.com>
2016-07-13 02:02:43 +0200

    trajectories and timeout and stuff

--- fonctions.js
+++ fonctions.js
@@ -1,18 +1,67 @@
 var nbStars = (window.innerHeight/100*window.innerWidth/10);
+var planets = [
+	{
+		name:"Mercure",
+		color:"#cc3300",
+		size:1
+	},{
+		name:"Venus",
+		color:"#ff9900",
+		size:1.1
+	},{
+		name:"Terre",
+		color:"#0099cc",
+		size:1.2
+	},{
+		name:"Mars",
+		color:"#ff3300",
+		size:1
+	},{
+		name:"Jupiter",
+		color:"#ffcc66",
+		size:2.4
+	},{
+		name:"Saturne",
+		color:"#ffff99",
+		size:2
+	},{
+		name:"Uranus",
+		color:"#00ccff",
+		size:1.2
+	},{
+		name:"Neptune",
+		color:"#33ccff",
+		size:1.1
+	},{
+		name:"Pluton",
+		color:"#993300",
+		size:0.5
+	},
+]
+
 
 function init(){
-	var can = document.getElementById('sky')
+	var can = document.getElementById('sky');
+	var p_can = document.getElementById('planetsPath');
 	putSize(can);
+	putSquareSize(p_can);
 	drawStars(can);
 	//drawPlanets(can);
 	//drawTarget(can);
 	addEvents(can);
-	movePlanets(can);
+	movePlanets(p_can);
 }
 
 function putSize(can){
 	can.height = window.innerHeight;
 	can.width = window.innerWidth;
+}
+
+function putSquareSize(can){
+	var l = window.innerHeight>window.innerWidth ? window.innerWidth : window.innerHeight;
+	console.log(l);
+	can.height = l;
+	can.width = l;
 }
 
 function drawStars(can){
@@ -64,7 +113,7 @@ function drawTarget(can){
 
 function drawPlanets(can,t){
 	var ctx = can.getContext("2d");
-	var size = can.width/50
+	var size = can.width/60
 
 	var initialGap = can.width/12;
 	var gap = ((can.width-initialGap)/2)/10
@@ -73,8 +122,8 @@ function drawPlanets(can,t){
 	var maxWidth = can.width;
 
 	for(var i=0;i<9;i++){
-		var x = (maxWidth/2)+(4+2*i)*Math.cos(Math.PI*t+i)*size;		// 4=magic number
-		var y = (maxHeight/2)+(4+2*i)*Math.sin(Math.PI*t+i)*size;		// 4=magic number
+		var x = (maxWidth/2)+(4+2*i)*Math.cos(Math.PI*t*(i+1))*size;		// 4=magic number
+		var y = (maxHeight/2)+(4+2*i)*Math.sin(Math.PI*t*(i+1))*size;		// 4=magic number
 
 		ctx.beginPath();
 		ctx.fillStyle = "#FFFFC4";
@@ -100,8 +149,9 @@ function addEvents(can){
 
 function movePlanets(can){
 	var t=0;
-	setInterval(function(){ 
+	setTimeout(function(){ 
+		can.getContext("2d").clearRect(0, 0, can.width, can.height);
 		drawPlanets(can,t);
-		t+=0.1;
-	}, 1000);
+		t+=0.02;
+	}, 100);
 }
